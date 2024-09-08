73bde849addb3a4d9ce7a97ccf5c5ef085a772f1
casdorio <casdorio@gmail.com>
2024-09-08 14:02:51 -0300

    step 1 start

---
+++ src/main/java/com/carlos/adriano/infnet/pb_tp2/PbTp2Application.java
@@ -0,0 +1,13 @@
+package com.carlos.adriano.infnet.pb_tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbTp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbTp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/adriano/infnet/pb_tp2/PbTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.adriano.infnet.pb_tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbTp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
